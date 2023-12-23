ba3d109ac8eb94393e9b843deecaf8163445e1c2
Rehman Nahimi <rehman2000@live.com>
2023-12-23 12:58:12 +0000

    Initial Commit

---
+++ Testable code/Testable_codeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testable_codeApp.swift
+//  Testable code
+//
+//  Created by Ray Nahimi on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Testable_codeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
